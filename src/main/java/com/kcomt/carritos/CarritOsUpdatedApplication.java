a2f83c3bbe2529d51cbfbbeeedf3a9c7df3fe32b
Luis Kcomt <luiskcomt@gmail.com>
2021-05-05 21:03:23 -0500

    creacion del proyecto

---
+++ src/main/java/com/kcomt/carritos/CarritOsUpdatedApplication.java
@@ -0,0 +1,13 @@
+package com.kcomt.carritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritOsUpdatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritOsUpdatedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcomt/carritos/CarritOsUpdatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcomt.carritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritOsUpdatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
